783f997e062d416410d8d81ff6902b9b6b49bfd6
RamintaRam <raminta5@yahoo.com>
2017-10-11 13:22:37 +0300

    changed title

--- resources/views/admin/core.blade.php
+++ resources/views/admin/core.blade.php
@@ -32,7 +32,7 @@
     {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">--}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Vilniaus gyventojai</title>
 
 </head>
 <body>
